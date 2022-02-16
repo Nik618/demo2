f188dd5d9cdd3fd5e4db03cfeb11042f304fc325
Nik618 <trinax01@gmail.com>
2022-02-16 14:13:29 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/Test.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Test {
+
+    @Bean
+    public static void test1() {
+        System.setProperty("javax.net.ssl.trustStore", "C:\\Program Files\\Java\\jdk-15.0.2\\lib\\security\\cacerts");
+        System.setProperty("javax.net.ssl.trustStorePassword", "changeit");
+    }
+}
